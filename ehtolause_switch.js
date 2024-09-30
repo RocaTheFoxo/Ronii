b2ec266fac135337837399063c8bc9417be15e44
OHKE 2023 <OHKE2023@D318.lao>
2024-09-30 12:30:39 +0300

    ehtolause

---
+++ ehtolause_switch.js
@@ -0,0 +1,18 @@
+let lang = "js";
+
+switch (lang) {
+    case "js":
+        console.log("Javascript");
+        break;
+    case "ja":
+        console.log("Java");
+        break;
+    case "sc":
+        console.log("Scala");
+        break;
+    case "py":
+        console.log("Python");
+        break;
+    default:
+        console.log("tuntematon kieli");
+}
