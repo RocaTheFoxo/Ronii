de29dd43bfa222747acc72fbb4e3636c19cbeaa7
OHKE 2023 <OHKE2023@D318.lao>
2024-09-30 09:53:02 +0300

    Toinen

---
+++ 1.js
@@ -0,0 +1,7 @@
+let ikä = 20;
+
+if (ikä >= 18) {
+    console.log("Olet täysi-ikäinen");
+} else {
+    console.log("Olet alaikäinen")
+}
---
+++ alkeistotietotyypit.js
@@ -0,0 +1,14 @@
+let isTrue = true;
+let isFalse = false;
+console.log('Boolean', isTrue, isFalse);
+
+let emptyValue = null
+console.log('null:', emptyValue);
+
+let notDefined;
+console.log('Undefined;', notDefined);
+
+let integer = 42;
+let decimal = 3.14;
+let notANumber = NaN;
+console.log('Number;', integer, decimal, notANumber);
