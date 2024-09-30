6f36e94b1ed4b713035c7171d601eeab9d2ab43c
OHKE 2023 <OHKE2023@D318.lao>
2024-09-30 12:11:59 +0300

    Yapadapaduu

---
+++ operators.js
@@ -0,0 +1,24 @@
+let x1 = 10;
+let y1 = 3;
+console.log(x1 + y1);
+console.log(x1 - y1);
+console.log(x1 * y1);
+console.log(x1 / y1);
+console.log(x1 % y1);
+
+let a1 = 5;
+let b1 = 10;
+console.log(a1 + b1);
+
+//postfix
+let x = 10;
+y = x++;
+console.log('y=${y}, x=${x}');1§
+
+var a = 20;
+b = ++a;
+
+let ikä = 20;
+if (ikä > 18 && ikä < 30) {
+    console.log("Olet nuori ihminen");
+}
