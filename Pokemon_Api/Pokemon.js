7a29794f177e8b672dc771b975742e8f77858625
OHKE 2023 <OHKE2023@D318.lao>
2024-10-02 12:29:24 +0300

    pokemon

--- Pokemon_Api/Pokemon.js
+++ Pokemon_Api/Pokemon.js
@@ -19,7 +19,8 @@ document.getElementById('pokemonForm').addEventListener('submit', function(event
             let pituus = data.height / 10;
             let id = data.id;
 
-            let evolutionUrl = `https://pokeapi.co/api/v2/evolution-chain/${id}/`
+            let evo1 = `https://pokeapi.co/api/v2/evolution-chain/${id+1}/`
+            let evo2 = `https://pokeapi.co/api/v2/evolution-chain/${id+2}/`
 
             document.getElementById('pokemonTiedot').innerHTML = `
                 <h1 class="pokemonclass">${nimi}</h1>
@@ -34,3 +35,43 @@ document.getElementById('pokemonForm').addEventListener('submit', function(event
             document.getElementById('pokemonTiedot').innerHTML = `<p>${error.message}</p>`
         });
 })
+
+/*function getevo(url) {
+    let data = fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Evoluutiota ei löytynyt.');
+            }
+            return response.json();
+        })
+        .then(data1 => {
+            console.log(data1.chain.evolves_to)
+            let name = data1.chain.evolves_to[0].species.name 
+
+            return name
+        })
+    
+        data.then(result => {
+            console.log(result)
+            let x = fetch(`https://pokeapi.co/api/v2/pokemon/${result}`)
+            .then(response2 => {
+                if (!response2.ok) {
+                    throw new Error('Pokémonia ei löytynyt.');
+                }
+                return response2.json();
+            })
+            .then(data2 => {
+                let nimi2 = data2.name
+                let kuva2 = data2.sprites.front_default
+
+                console.log(kuva2)
+                //document.getElementById('pokemonTiedot').innerHTML = document.getElementById('pokemonTiedot').innerHTML+`<img class="pokemonimage2" src="${kuva2}" alt="${nimi2}>`
+                let img = document.createElement("img")
+                img.className = "pokemonimage1"
+                img.src = kuva2
+                img.alt = nimi2
+
+                document.getElementById('pokemonTiedot').appendChild(img)
+            })
+        })
+}*/
