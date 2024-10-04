afc838766f509c0c00d54f79221e80551fc6e0b5
OHKE 2023 <OHKE2023@D318.lao>
2024-10-04 11:01:00 +0300

    Genshin

--- genshin/genshin.js
+++ genshin/genshin.js
@@ -156,3 +156,237 @@ document.getElementById("getconsumable").addEventListener('submit', function(e) 
         })
     })
 })
+
+document.getElementById("getdomain").addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    let domain = document.getElementById("domains1").value.toLowerCase();
+    let apiUrl5 = `https://genshin.jmp.blue/domains/${domain}`
+
+    fetch (apiUrl5)
+     .then (response => {
+        if (!response.ok) {
+            throw new Error ("Domain not found")
+        }
+        return response.json();
+     })
+
+     .then (data => {
+        let name = data.name
+        let description = data.description
+        let location = data.location
+        let nation = data.nation
+        let type = data.type
+        let requirements = data.requirements[0].level
+        let adventurerank = data.requirements[0].adventureRank
+        let requirements1 = data.requirements[1].level
+        let adventurerank1 = data.requirements[1].adventureRank
+        let requirements2 = data.requirements[2].level
+        let adventurerank2 = data.requirements[2].adventureRank
+        let requirements3 = data.requirements[3].level
+        let adventurerank3 = data.requirements[3].adventureRank
+        let elements = data.recommendedElements[0]
+        let elements1 = data.recommendedElements[1]
+        let elements2 = data.recommendedElements[2]
+
+        document.getElementById("domainInfo").innerHTML = `
+            <h2>${name}</h2>
+            <p>${description}</p>
+            <h2>Location: ${location}</h2>
+            <h2>Nation: ${nation}</h2>
+            <h2>Type: ${type}</h2>
+        `
+
+        document.getElementById("domainInfo2").innerHTML = `
+            <h2>Level: ${requirements}. Adventure rank: ${adventurerank}</h2>
+            <h2>Level: ${requirements1}. Adventure rank: ${adventurerank1}</h2>
+            <h2>Level: ${requirements2}. Adventure rank: ${adventurerank2}</h2>
+            <h2>Level: ${requirements3}. Adventure rank: ${adventurerank3}</h2>
+            <h2>Recommended Elements: ${elements}, ${elements1} ${elements2}</h2>
+        `
+     })
+})
+
+document.getElementById("getelement").addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let element = document.getElementById("elements1").value.toLowerCase();
+    let ApiUrl6 = `https://genshin.jmp.blue/elements/${element}`
+
+    fetch (ApiUrl6)
+     .then (response => {
+        if (!response.ok) {
+            throw new Error ("No element found")
+        }
+        return response.json()
+     })
+
+     .then (data => {
+        console.log(data)
+
+        let name = data.name
+        /*let desc = data.reactions[0].description
+        let reaction = data.reactions[0].name
+        let desc1 = data.reactions[1].description || "None"
+        let reaction1 = data.reactions[1].name || "None"
+        let desc2 = data.reactions[2].description || "None"
+        let reaction2 = data.reactions[2].name || "None"*/
+
+        data.reactions.forEach(function(a, i) {
+            let name = document.createElement("h2")
+            name.innerHTML = a.name
+
+            let desc = document.createElement("h2")
+            desc.innerHTML = a.description
+            desc.className = "desci"
+
+            document.getElementById("elementinfo").appendChild(name)
+            document.getElementById("elementinfo").appendChild(desc)
+        })
+
+        /*document.getElementById("elementinfo").innerHTML = `
+            <h2>${name}</h2>
+            <h2>${desc}</h2>
+            <h2>Element Reaction: ${reaction}</h2>
+        `
+
+        document.getElementById("elementinfo1").innerHTML = `
+            <h2>${desc1}</h2>
+            <h2>Reaction ${reaction1}</h2>
+        `
+
+        document.getElementById("elementinfo2").innerHTML = `
+            <h2>${desc2}</h2>
+            <h2>Reaction ${reaction2}</h2>
+        `*/
+     })
+     .catch (error => {
+        console.log(error)
+     })
+})
+
+document.getElementById("getenemy").addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let enemy = document.getElementById("enemies1").value.toLowerCase();
+    let ApiUrl7 = `https://genshin.jmp.blue/enemies/${enemy}`
+
+    fetch (ApiUrl7)
+     .then (response => {
+        if (!response.ok) {
+            throw new Error ("No Enemy Found")
+        }
+        return response.json();
+     })
+
+     .then (data => {
+        console.log(data)
+
+        let name = data.name
+        let faction = data.faction
+        let family = data.family
+        let region = data.region
+        let type = data.type
+        let desc = data.description
+
+        if (data.descriptions != null) {
+            data.descriptions.forEach(function(a, i) {
+                let name = document.createElement("h2")
+                name.innerHTML = a.name
+                document.getElementById("enemyinfo1").appendChild(name)
+                name.className = "name1"
+    
+                let desc = document.createElement("p")
+                desc.innerHTML = a.description
+                document.getElementById("enemyinfo1").appendChild(desc)
+    
+                console.log(a)
+            }) 
+        }
+        else {
+            document.getElementById("enemyinfo1").innerHTML = "";
+        }
+
+        document.getElementById("enemyinfo").innerHTML = `
+            <h2>${name}</h2>
+            <h2>Faction: ${faction}</h2>
+            <h2>Family: ${family}</h2>
+            <h2>Region: ${region}</h2>
+            <h2>Type: ${type}</h2>
+            <p>${desc}</p>
+        `
+     })
+})
+
+document.getElementById("getmaterial").addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    let material = document.getElementById("materials1").value.toLowerCase();
+    let ApiUrl8 = `https://genshin.jmp.blue/materials`
+
+    fetch(ApiUrl8)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(function(v) {
+            fetch(ApiUrl8+"/"+v)
+            .then(response1 => response1.json())
+            .then(data1 => {
+                if (data1[material]) {
+                    console.log(data1[material])
+
+                    let mat = data1[material]
+
+                    let a = JSON.stringify(mat).toLocaleLowerCase()
+
+                    for (i=0; i<=a.length; i++) {
+                        a = a.replace("{", "")
+                        a = a.replace("}", "")
+                        a = a.replace('"', "")
+                        a = a.replace("[", "")
+                        a = a.replace("]", "")
+                        a = a.replace(":", "")
+                        a = a.replace(",", " | ")
+                        a = a.replace("characters", "")
+                        a = a.replace("availability", "")
+                        a = a.replace("source", "")
+                        a = a.replace("name", "")
+                        a = a.replace("rarity", "")
+                        a = a.replace("id", "")
+                        a = a.replace("experience", "")
+                        a = a.replace("weapons", "")
+                        a = a.replace("items", "")
+                    }
+
+                    document.getElementById("materialinfo").innerHTML = `<p> ${a} </p>`
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        })
+    })
+})
+
+document.getElementById("getnation").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let nation = document.getElementById("nations1").value.toLowerCase();
+    let ApiUrl9 = `https://genshin.jmp.blue/nations`
+
+    fetch(ApiUrl9)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error ("Invalid Nation")
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.forEach(function(v) {
+                fetch(ApiUrl9+"/"+v)
+                .then(response1 => response1.json())
+                .then(data1 => {
+                    console.log(data1)
+                })
+            })
+        })
+})
